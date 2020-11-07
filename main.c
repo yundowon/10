f817180a7f7c53ee967d43f30a21324aa5af6f4c
yundowon <70631088+yundowon@users.noreply.github.com>
2020-11-08 05:05:16 +0900

    02

--- main.c
+++ main.c
@@ -6,12 +6,20 @@
 int main(int argc, char *argv[]) {
 	
 	int i = 10;
-	char c = 69;
-	float f = 12.3;
+	char c = 'a';
+
+	int *iptr;
+	iptr = &i;
+
+	char *cptr;
+	cptr = &c;
 	
-	printf("i: %i %p \n", i, &i);
-	printf("c: %c %p \n", c, &c);
-	printf("f: %f %p \n", f, &f);
+	int *iptr2;
+	iptr2 = iptr;
+	
+	printf("i : %p\n %p(size: %i)\n", iptr, &i, sizeof(iptr));
+	printf("c : %p\n %p(size: %i)\n", cptr, &c, sizeof(cptr));
+	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
 	
 	return 0;
 }
